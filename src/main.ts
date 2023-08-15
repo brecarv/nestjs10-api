b617e4e09bbfd7ad693902d8ba09777810300e60
Emerson Carvalho <brecarv@outlook.com>
2023-08-15 17:09:55 -0300

    Part two: validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exceptions-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new PrismaExceptionFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }),
+  );
 
   await app.listen(3000);
 }
